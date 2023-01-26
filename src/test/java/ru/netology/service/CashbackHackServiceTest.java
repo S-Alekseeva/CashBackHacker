05406bb58c06b183ac8739e58010171206d7f9da
Светлана Алексеева <Lanaleks90@gmail.com>
2023-01-26 13:31:45 +0400

    Add branch junit4-platform

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,6 +1,7 @@
 package ru.netology.service;
 
-import org.junit.Test;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.Assert.*;
 
@@ -8,20 +9,20 @@ public class CashbackHackServiceTest {
 
     CashbackHackService service = new CashbackHackService();
 
-    @Test
-    public void testBoundaryValue999() {
+   @Test
+   public void testBoundaryValue999() {
 
-        int expected = 1;
-        int actual = service.remain(999);
-        assertEquals(expected, actual);
-    }
+       int expected = 1;
+       int actual = service.remain(999);
+       Assert.assertEquals(expected, actual);
+   }
 
     @Test
     public void TestBoundaryValue1001() {
 
         int expected = 999;
         int actual = service.remain(1001);
-        assertEquals(expected, actual);
+        Assert.assertEquals(expected, actual);
     }
 
     @Test
@@ -29,7 +30,7 @@ public class CashbackHackServiceTest {
 
         int expected = 0;
         int actual = service.remain(1000);
-        assertEquals(expected, actual);
+        Assert.assertEquals(expected, actual);
     }
 
     @Test
@@ -37,6 +38,6 @@ public class CashbackHackServiceTest {
 
         int expected = 1000;
         int actual = service.remain(0);
-        assertEquals(expected, actual);
+        Assert.assertEquals(expected, actual);
     }
 }
